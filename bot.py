9098f35c9bbce6f8ad004676713e8398526b1eee
DAlnAO <2332176641@qq.com>
2025-02-11 11:28:53 +0800

    更新 bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ logging.basicConfig(filename='trading_bot.log', level=logging.INFO, format='%(as
 exchange = ccxt.okx({
     'apiKey': "0f046e6a-1627-4db4-b97d-083d7e6cc16b",
     'secret': "BF7BC880C73AD54D2528FA271A358C2C",
-    'password': "Duan0918",
+    'password': "Duan0918.",
     'options': {'defaultType': 'swap'},
 })
 
